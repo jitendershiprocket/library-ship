ef6c395d8e74cacd05a119e457aa4d0d2f4c9696
jitendershiprocket <jitender.sharma@kartrocket.com>
2024-12-09 13:42:38 +0530

    initial commit

---
+++ projects/switcher/src/lib/switcher.module.ts
@@ -0,0 +1,10 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SwitcherComponent } from './switcher.component';
+
+@NgModule({
+  declarations: [SwitcherComponent],
+  imports: [CommonModule],
+  exports: [SwitcherComponent], // Export the component for use in other modules
+})
+export class SwitcherModule {}
